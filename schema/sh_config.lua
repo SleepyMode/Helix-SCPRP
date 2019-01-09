0e1f8b4419d423ba7eea7697fca9182b8006a144
SleepyMode <nivnuv10@gmail.com>
2019-01-09 19:45:27 +0200

    Include configuration

---
+++ schema/sh_config.lua
@@ -0,0 +1,16 @@
+
+-- Currency configuration
+ix.currency.symbol = "$"
+ix.currency.singular = "USD"
+ix.currency.singular = "USD"
+
+-- Make it so that characters are considered
+-- recognised in the scoreboard by default.
+-- This will make players be able to see
+-- the names and avatars(models) of other
+-- players in the scoreboard, but doesn't
+-- mean that the players actually recognise
+-- the characters, so that when interacting
+-- in character it would still show others
+-- as unrecognised.
+ix.config.SetDefault("scoreboardRecognition", true)
--- schema/sh_schema.lua
+++ schema/sh_schema.lua
@@ -2,3 +2,5 @@
 Schema.name = "SCP RP"
 Schema.author = "SleepyMode"
 Schema.description = "A schema set in the SCP Universe."
+
+ix.util.Include("sh_config.lua")
