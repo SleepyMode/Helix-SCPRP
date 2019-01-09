25d0c4914e8558d7ed72dcb20a63d18668a707cf
SleepyMode <nivnuv10@gmail.com>
2019-01-09 19:23:23 +0200

    Add Mobile Task Force faction

---
+++ schema/factions/sh_mtf.lua
@@ -0,0 +1,7 @@
+
+FACTION.name = "Mobile Task Force"
+FACTION.description = "The best specialists of the foundation, trained to handle the most extreme of situations."
+FACTION.color = Color(70, 50, 150)
+FACTION.isDefault = false
+
+FACTION_MTF = FACTION.index
