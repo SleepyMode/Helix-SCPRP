bfb73824e270979f8c04b3b7d05b77ec480adeaa
SleepyMode <sleepy@sleepymode.me>
2019-02-16 21:17:57 +0200

    Set TRU models

--- schema/factions/sh_tru.lua
+++ schema/factions/sh_tru.lua
@@ -4,4 +4,8 @@ FACTION.description = "Highly trained and heavily armed units ready to deploy on
 FACTION.color = Color(40, 80, 180)
 FACTION.isDefault = false
 
+FACTION.models = {
+	"models/scp_operators/scp_security_heavy_f_npc.mdl"
+}
+
 FACTION_TRU = FACTION.index
