66ef5a0bbaae89440f0c52bc24de9bb447ba7fec
SleepyMode <sleepy@sleepymode.me>
2019-02-16 21:16:13 +0200

    Scale salary by clearance level

---
+++ schema/sv_hooks.lua
@@ -0,0 +1,12 @@
+
+local Schema = Schema
+
+function Schema:GetSalaryAmount(client, faction)
+	local pay = faction.pay
+	local character = client:GetCharacter()
+	local clearance = character:GetClearance()
+
+	if (clearance > 1) then
+		local pay = pay * clearance * 2.5
+	end
+end
