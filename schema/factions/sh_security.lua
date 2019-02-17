518ec4496562e1e242d425628079b7934be0ac4e
SleepyMode <sleepy@sleepymode.me>
2019-02-17 21:04:18 +0200

    Security Models

--- schema/factions/sh_security.lua
+++ schema/factions/sh_security.lua
@@ -4,4 +4,16 @@ FACTION.description = "Personnel tasked with guarding the facility and handling 
 FACTION.color = Color(50, 120, 255)
 FACTION.isDefault = true
 
+FACTION.models = {
+	"models/player/guard_pack/guard_01.mdl",
+	"models/player/guard_pack/guard_02.mdl",
+	"models/player/guard_pack/guard_03.mdl",
+	"models/player/guard_pack/guard_04.mdl",
+	"models/player/guard_pack/guard_05.mdl",
+	"models/player/guard_pack/guard_06.mdl",
+	"models/player/guard_pack/guard_07.mdl",
+	"models/player/guard_pack/guard_08.mdl",
+	"models/player/guard_pack/guard_09.mdl"	
+}
+
 FACTION_SECURITY = FACTION.index
