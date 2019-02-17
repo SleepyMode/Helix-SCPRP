a488cd522b67bb07d2fb4b4d6b77cffd8ffade85
SleepyMode <sleepy@sleepymode.me>
2019-02-17 21:04:24 +0200

    MTF Models

--- schema/factions/sh_mtf.lua
+++ schema/factions/sh_mtf.lua
@@ -4,4 +4,23 @@ FACTION.description = "The best specialists of the foundation, trained to handle
 FACTION.color = Color(70, 50, 150)
 FACTION.isDefault = false
 
+FACTION.models = {
+	-- Male
+	"models/multicamamericans/soldiers/male/male_01_npc.mdl",
+	"models/multicamamericans/soldiers/male/male_02_npc.mdl",
+	"models/multicamamericans/soldiers/male/male_03_npc.mdl",
+	"models/multicamamericans/soldiers/male/male_04_npc.mdl",
+	"models/multicamamericans/soldiers/male/male_05_npc.mdl",
+	"models/multicamamericans/soldiers/male/male_06_npc.mdl",
+	"models/multicamamericans/soldiers/male/male_07_npc.mdl",
+	"models/multicamamericans/soldiers/male/male_08_npc.mdl",
+	"models/multicamamericans/soldiers/male/male_09_npc.mdl",
+
+	-- Female
+	"models/multicamamericans/soldiers/female/female_01_npc.mdl",
+	"models/multicamamericans/soldiers/female/female_02_npc.mdl",
+	"models/multicamamericans/soldiers/female/female_04_npc.mdl",
+	"models/multicamamericans/soldiers/female/female_06_npc.mdl"
+}
+
 FACTION_MTF = FACTION.index
