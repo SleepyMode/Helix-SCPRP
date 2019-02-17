47e7cddaa67710249f65549b5a084d5e997a3d96
SleepyMode <sleepy@sleepymode.me>
2019-02-17 20:31:52 +0200

    Add Server Administration faction

---
+++ schema/factions/sh_serveradmin.lua
@@ -0,0 +1,7 @@
+
+FACTION.name = "Server Administration"
+FACTION.description = "Server staff."
+FACTION.color = Color(102, 51, 153, 255)
+FACTION.isDefault = false
+
+FACTION_SERVERADMIN = FACTION.index
