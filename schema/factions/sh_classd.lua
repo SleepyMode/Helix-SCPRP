b6981a6451b4730e19cf093163978156446fefcb
SleepyMode <sleepy@sleepymode.me>
2019-02-17 21:04:12 +0200

    Class D models

--- schema/factions/sh_classd.lua
+++ schema/factions/sh_classd.lua
@@ -4,4 +4,25 @@ FACTION.description = "Disposeable subjects used for handling hazardous situatio
 FACTION.color = Color(255, 150, 20)
 FACTION.isDefault = false
 
+FACTION.models = {
+	-- Male
+	"models/humans/orange1/player/male_01.mdl",
+	"models/humans/orange1/player/male_02.mdl",
+	"models/humans/orange1/player/male_03.mdl",
+	"models/humans/orange1/player/male_04.mdl",
+	"models/humans/orange1/player/male_05.mdl",
+	"models/humans/orange1/player/male_06.mdl",
+	"models/humans/orange1/player/male_07.mdl",
+	"models/humans/orange1/player/male_08.mdl",
+	"models/humans/orange1/player/male_09.mdl",
+
+	-- Female
+	"models/humans/orange1/player/female_01.mdl",
+	"models/humans/orange1/player/female_02.mdl",
+	"models/humans/orange1/player/female_03.mdl",
+	"models/humans/orange1/player/female_04.mdl",
+	"models/humans/orange1/player/female_06.mdl",
+	"models/humans/orange1/player/female_07.mdl"
+}
+
 FACTION_CLASSD = FACTION.index
