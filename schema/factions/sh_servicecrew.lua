af20689513c12d4af8a8ddb72d94d6f2137ac1da
SleepyMode <sleepy@sleepymode.me>
2019-02-19 21:13:15 +0200

    Update Service Crew Models

--- schema/factions/sh_servicecrew.lua
+++ schema/factions/sh_servicecrew.lua
@@ -4,4 +4,13 @@ FACTION.description = "Personnel tasked with service tasks, such as janitors."
 FACTION.color = Color(150, 80, 0)
 FACTION.isDefault = true
 
+FACTION.models = {
+	"models/player/kerry/class_jan_2.mdl",
+	"models/player/kerry/class_jan_3.mdl",
+	"models/player/kerry/class_jan_4.mdl",
+	"models/player/kerry/class_jan_5.mdl",
+	"models/player/kerry/class_jan_6.mdl",
+	"models/player/kerry/class_jan_7.mdl"
+}
+
 FACTION_SERVICECREW = FACTION.index
