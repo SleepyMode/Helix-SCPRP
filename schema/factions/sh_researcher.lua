c19a02955d372249213bf8bd362d67a4661fbb6a
SleepyMode <sleepy@sleepymode.me>
2019-02-19 21:15:39 +0200

    Update Researcher models

--- schema/factions/sh_researcher.lua
+++ schema/factions/sh_researcher.lua
@@ -4,4 +4,26 @@ FACTION.description = "Scientists researching SCPs and anomalies."
 FACTION.color = Color(255, 200, 50)
 FACTION.isDefault = true
 
+FACITON.models = {
+	-- Male
+	"models/bmscientistcits/male_01.mdl",
+	"models/bmscientistcits/male_02.mdl",
+	"models/bmscientistcits/male_03.mdl",
+	"models/bmscientistcits/male_04.mdl",
+	"models/bmscientistcits/male_05.mdl",
+	"models/bmscientistcits/male_06.mdl",
+	"models/bmscientistcits/male_07.mdl",
+	"models/bmscientistcits/male_08.mdl",
+	"models/bmscientistcits/male_09.mdl",
+	"models/bmscientistcits/male_10.mdl"
+
+	-- Female
+	"models/bmscientistcits/female_01.mdl",
+	"models/bmscientistcits/female_02.mdl",
+	"models/bmscientistcits/female_03.mdl",
+	"models/bmscientistcits/female_04.mdl",
+	"models/bmscientistcits/female_06.mdl",
+	"models/bmscientistcits/female_07.mdl"
+}
+
 FACTION_RESEARCH = FACTION.index
