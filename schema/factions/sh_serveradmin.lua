1ecc4e89e2b0c5b6e495b660a704208177fba3f4
SleepyMode <sleepy@sleepymode.me>
2019-02-23 14:15:42 +0200

    Server Admin models

--- schema/factions/sh_serveradmin.lua
+++ schema/factions/sh_serveradmin.lua
@@ -4,4 +4,66 @@ FACTION.description = "Server staff."
 FACTION.color = Color(102, 51, 153, 255)
 FACTION.isDefault = false
 
+FACTION.models = {
+	"models/suits/male_01_closed_coat_tie.mdl",
+	"models/suits/male_02_closed_coat_tie.mdl",
+	"models/suits/male_03_closed_coat_tie.mdl",
+	"models/suits/male_04_closed_coat_tie.mdl",
+	"models/suits/male_05_closed_coat_tie.mdl",
+	"models/suits/male_06_closed_coat_tie.mdl",
+	"models/suits/male_07_closed_coat_tie.mdl",
+	"models/suits/male_08_closed_coat_tie.mdl",
+	"models/suits/male_09_closed_coat_tie.mdl",
+
+	"models/suits/male_01_closed_tie.mdl",
+	"models/suits/male_02_closed_tie.mdl",
+	"models/suits/male_03_closed_tie.mdl",
+	"models/suits/male_04_closed_tie.mdl",
+	"models/suits/male_05_closed_tie.mdl",
+	"models/suits/male_06_closed_tie.mdl",
+	"models/suits/male_07_closed_tie.mdl",
+	"models/suits/male_08_closed_tie.mdl",
+	"models/suits/male_09_closed_tie.mdl",
+
+	"models/suits/male_01_open.mdl",
+	"models/suits/male_02_open.mdl",
+	"models/suits/male_03_open.mdl",
+	"models/suits/male_04_open.mdl",
+	"models/suits/male_05_open.mdl",
+	"models/suits/male_06_open.mdl",
+	"models/suits/male_07_open.mdl",
+	"models/suits/male_08_open.mdl",
+	"models/suits/male_09_open.mdl",
+
+	"models/suits/male_01_open_waistcoat.mdl",
+	"models/suits/male_02_open_waistcoat.mdl",
+	"models/suits/male_03_open_waistcoat.mdl",
+	"models/suits/male_04_open_waistcoat.mdl",
+	"models/suits/male_05_open_waistcoat.mdl",
+	"models/suits/male_06_open_waistcoat.mdl",
+	"models/suits/male_07_open_waistcoat.mdl",
+	"models/suits/male_08_open_waistcoat.mdl",
+	"models/suits/male_09_open_waistcoat.mdl",
+
+	"models/suits/male_01_shirt.mdl",
+	"models/suits/male_02_shirt.mdl",
+	"models/suits/male_03_shirt.mdl",
+	"models/suits/male_04_shirt.mdl",
+	"models/suits/male_05_shirt.mdl",
+	"models/suits/male_06_shirt.mdl",
+	"models/suits/male_07_shirt.mdl",
+	"models/suits/male_08_shirt.mdl",
+	"models/suits/male_09_shirt.mdl",
+
+	"models/suits/male_01_shirt_tie.mdl",
+	"models/suits/male_02_shirt_tie.mdl",
+	"models/suits/male_03_shirt_tie.mdl",
+	"models/suits/male_04_shirt_tie.mdl",
+	"models/suits/male_05_shirt_tie.mdl",
+	"models/suits/male_06_shirt_tie.mdl",
+	"models/suits/male_07_shirt_tie.mdl",
+	"models/suits/male_08_shirt_tie.mdl",
+	"models/suits/male_09_shirt_tie.mdl"
+}
+
 FACTION_SERVERADMIN = FACTION.index
