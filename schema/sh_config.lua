a77c905e7c442bc4fc21d068f1ec8f86b50d5cff
SleepyMode <sleepy@sleepymode.me>
2019-02-23 19:55:42 +0200

    Remove unneeded things

--- schema/sh_config.lua
+++ schema/sh_config.lua
@@ -3,15 +3,3 @@
 ix.currency.symbol = "$"
 ix.currency.singular = "USD"
 ix.currency.singular = "USD"
-
--- Make it so that characters are considered
--- recognised in the scoreboard by default.
--- This will make players be able to see
--- the names and avatars(models) of other
--- players in the scoreboard, but doesn't
--- mean that the players actually recognise
--- the characters, so that when interacting
--- in character it would still show others
--- as unrecognised.
--- NOTE: This is no longer the case.
--- ix.config.SetDefault("scoreboardRecognition", true)
