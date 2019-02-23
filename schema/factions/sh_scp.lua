841755e178d3c8fc694d5f19c67e17bfacc9fd13
SleepyMode <sleepy@sleepymode.me>
2019-02-23 20:50:36 +0200

    Add SCP faction

---
+++ schema/factions/sh_scp.lua
@@ -0,0 +1,15 @@
+
+FACTION.name = "SCP"
+FACTION.description = "An anomalous object contained by the foundation."
+FACTION.color = Color(150, 0, 0)
+FACTION.isDefault = false
+
+FACTION.models = {
+	"models/vinrax/scp173/scp173.mdl",
+	"models/vinrax/player/scp106_player.mdl",
+	"models/vinrax/scp096/scp96.mdl",
+	"models/vinrax/player/scp049_player.mdl",
+	"models/vinrax/player/035_player.mdl"
+}
+
+FACTION_SCP = FACTION.index
