c6b3a7e4e011b911c9f4d7cceb2fa1bf7095fa59
SleepyMode <sleepy@sleepymode.me>
2019-03-01 13:41:35 +0200

    Fix MTF not showing to other MTF/self

--- schema/cl_hooks.lua
+++ schema/cl_hooks.lua
@@ -2,7 +2,7 @@
 local Schema = Schema
 
 function Schema:ShouldShowPlayerOnScoreboard(client)
-	if (client:Team() == FACTION_MTF) then
+	if (client:Team() == FACTION_MTF and LocalPlayer():Team() != FACTION_MTF) then
 		return false
 	end
 end
