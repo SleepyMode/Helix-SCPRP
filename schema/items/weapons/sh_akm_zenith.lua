d483ae7a01c69d415f4d1685f68fc98f01b985a9
SleepyMode <nivnuv10@gmail.com>
2019-04-15 11:55:32 +0300

    Update AKM Zenith description

--- schema/items/weapons/sh_akm_zenith.lua
+++ schema/items/weapons/sh_akm_zenith.lua
@@ -1,6 +1,6 @@
 
 ITEM.name = "AKM Zenith"
-ITEM.description = "The AKM, or Klashnikov Modernized Automatic Rifle, is an updated variant of the classic AK-47. Produced since 1959, it offers a reliable design, a powerful cartridge, and a moderate rate of fire. ."
+ITEM.description = "The AKM, or Klashnikov Modernized Automatic Rifle, is an updated variant of the classic AK-47. Produced since 1959, it offers a reliable design, a powerful cartridge, and a moderate rate of fire."
 ITEM.model = Model("models/weapons/tfa_ins2/w_akz.mdl")
 ITEM.class = "tfa_ins2_akz"
 ITEM.weaponCategory = "primary"
